c9700b5ac042d098e6ad7a7dacc65492335275ab
Yanina.Matuzava <yaninochka2008@gmail.com>
2023-09-25 16:24:10 +0300

    Lesson2

---
+++ src/MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("*  *  *");
+        System.out.println(" * * * ");
+        System.out.println("  ***  ");
+        System.out.println("*******");
+        System.out.println("  ***  ");
+        System.out.println(" * * * ");
+        System.out.println("*  *  *");
+    }
+}
